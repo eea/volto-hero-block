1a51209dbaec1b9aa1a7e16f3d997dcf5fa8f764
David Ichim <ichim.david@gmail.com>
2023-02-02 19:54:26 +0200

    lint fix

--- src/components/Blocks/Hero/schema.js
+++ src/components/Blocks/Hero/schema.js
@@ -1,4 +1,4 @@
-import { addStyling } from "@plone/volto/helpers";
+import { addStyling } from '@plone/volto/helpers';
 import config from '@plone/volto/registry';
 
 import alignTopSVG from '@plone/volto/icons/move-up.svg';
@@ -13,84 +13,84 @@ const ALIGN_INFO_MAP = {
 
 export default () => {
   return {
-    title: "Hero",
+    title: 'Hero',
     fieldsets: [
       {
-        id: "default",
-        title: "Default",
+        id: 'default',
+        title: 'Default',
         fields: [
-          "fullWidth",
-          "fullHeight",
-          "quoted",
-          "spaced",
-          "inverted",
-          "buttonLabel",
-          "buttonLink",
-          "overlay",
-          "image",
+          'fullWidth',
+          'fullHeight',
+          'quoted',
+          'spaced',
+          'inverted',
+          'buttonLabel',
+          'buttonLink',
+          'overlay',
+          'image',
         ],
       },
       {
-        id: "copyright",
-        title: "Copyright",
-        fields: ["copyright", "copyrightIcon", "copyrightPosition"],
+        id: 'copyright',
+        title: 'Copyright',
+        fields: ['copyright', 'copyrightIcon', 'copyrightPosition'],
       },
     ],
     properties: {
       fullWidth: {
-        title: "Full width",
-        type: "boolean",
+        title: 'Full width',
+        type: 'boolean',
         defaultValue: true,
       },
       fullHeight: {
-        title: "Full height",
-        type: "boolean",
+        title: 'Full height',
+        type: 'boolean',
         defaultValue: true,
       },
       quoted: {
-        title: "Quoted",
-        type: "boolean",
+        title: 'Quoted',
+        type: 'boolean',
         defaultValue: false,
       },
       spaced: {
-        title: "Spaced",
-        type: "boolean",
+        title: 'Spaced',
+        type: 'boolean',
         defaultValue: false,
       },
       inverted: {
-        title: "Inverted",
-        type: "boolean",
+        title: 'Inverted',
+        type: 'boolean',
         defaultValue: true,
       },
       buttonLabel: {
-        title: "Button label",
-        widget: "textarea",
+        title: 'Button label',
+        widget: 'textarea',
       },
       buttonLink: {
-        title: "Button link",
-        widget: "url",
+        title: 'Button link',
+        widget: 'url',
       },
       overlay: {
-        title: "Image darken overlay",
-        type: "boolean",
+        title: 'Image darken overlay',
+        type: 'boolean',
         defaultValue: true,
       },
       image: {
-        title: "Image",
-        widget: "attachedimage",
+        title: 'Image',
+        widget: 'attachedimage',
       },
       copyright: {
-        title: "Text",
+        title: 'Text',
       },
       copyrightIcon: {
-        title: "Icon",
-        default: "ri-copyright-line",
+        title: 'Icon',
+        default: 'ri-copyright-line',
       },
       copyrightPosition: {
-        title: "Align",
-        widget: "align",
-        actions: ["left", "right"],
-        defaultValue: "left",
+        title: 'Align',
+        widget: 'align',
+        actions: ['left', 'right'],
+        defaultValue: 'left',
       },
     },
     required: [],
@@ -100,12 +100,12 @@ export default () => {
 export const stylingSchema = (props) => {
   const schema = addStyling(props);
   schema.properties.styles.schema = {
-    title: "Hero style",
-    block: "hero",
+    title: 'Hero style',
+    block: 'hero',
     fieldsets: [
       {
-        id: "default",
-        title: "Default",
+        id: 'default',
+        title: 'Default',
         fields: [
           'backgroundVariant',
           'bg',
@@ -126,8 +126,8 @@ export const stylingSchema = (props) => {
         defaultValue: 'has--bg--center',
       },
       backgroundVariant: {
-        title: "Background theme",
-        widget: "theme_picker",
+        title: 'Background theme',
+        widget: 'theme_picker',
         colors: [
           ...(config.settings && config.settings.themeColors
             ? config.settings.themeColors.map(({ value, title }) => ({
@@ -139,22 +139,22 @@ export const stylingSchema = (props) => {
         ],
       },
       alignContent: {
-        title: "Content align",
+        title: 'Content align',
         choices: [
-          ["start", "Top"],
-          ["center", "Center"],
-          ["end", "Bottom"],
+          ['start', 'Top'],
+          ['center', 'Center'],
+          ['end', 'Bottom'],
         ],
       },
       textAlign: {
-        title: "Text align",
-        widget: "align",
-        actions: ["left", "center", "right"],
-        defaultValue: "left",
+        title: 'Text align',
+        widget: 'align',
+        actions: ['left', 'center', 'right'],
+        defaultValue: 'left',
       },
       textVariant: {
-        title: "Text theme",
-        widget: "theme_picker",
+        title: 'Text theme',
+        widget: 'theme_picker',
         colors: [
           ...(config.settings && config.settings.themeColors
             ? config.settings.themeColors.map(({ value, title }) => ({
@@ -166,8 +166,8 @@ export const stylingSchema = (props) => {
         ],
       },
       buttonVariant: {
-        title: "Button theme",
-        widget: "theme_picker",
+        title: 'Button theme',
+        widget: 'theme_picker',
         colors: [
           ...(config.settings && config.settings.themeColors
             ? config.settings.themeColors.map(({ value, title }) => ({
@@ -179,10 +179,10 @@ export const stylingSchema = (props) => {
         ],
       },
       buttonAlign: {
-        title: "Button align",
-        widget: "align",
-        actions: ["left", "center", "right"],
-        defaultValue: "left",
+        title: 'Button align',
+        widget: 'align',
+        actions: ['left', 'center', 'right'],
+        defaultValue: 'left',
       },
     },
     required: [],
