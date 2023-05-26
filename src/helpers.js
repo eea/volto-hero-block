8c9ee980e898cb09a553f5956871c0b285c80280
Miu Razvan <miu.razvan28@gmail.com>
2023-05-26 14:22:29 +0300

    other: update getFieldURL function

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 import { serializeNodes } from '@plone/volto-slate/editor/render';
-import { isArray } from 'lodash';
+import isArray from 'lodash/isArray';
 import isObject from 'lodash/isObject';
 import isString from 'lodash/isString';
 import { isInternalURL, flattenToAppURL } from '@plone/volto/helpers';
@@ -8,9 +8,9 @@ export const getFieldURL = (data) => {
   let url = data;
   const _isObject = data && isObject(data) && !isArray(data);
   if (_isObject && data['@type'] === 'URL') {
-    url = data['value'] || data['url'] || data['href'] || data;
+    url = data['value'] ?? data['url'] ?? data['href'] ?? data;
   } else if (_isObject) {
-    url = data['@id'] || data['url'] || data['href'] || data;
+    url = data['@id'] ?? data['url'] ?? data['href'] ?? data;
   }
   if (isArray(data)) {
     url = data.map((item) => getFieldURL(item));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import installHeroBlock from '@eeacms/volto-hero-block/components/Blocks/Hero';
+// import { getFieldURL } from '@eeacms/volto-hero-block/helpers';
 
 const applyConfig = (config) => {
+  // Passing a string url expect a relative string path
+  // console.log('HERE', getFieldURL('/path/to/image'));
+  // Passing a string url with absolute path expect a relativ string path
+  // console.log('HERE', getFieldURL('/path/to/image'));
   return [installHeroBlock].reduce((acc, apply) => apply(acc), config);
 };
 
