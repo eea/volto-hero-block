5a25a5d1e213488c84bee19118d86646592371ed
Cretu Mihaela <cretu.mihaelaa11@gmail.com>
2023-10-17 18:21:00 +0300

    increase coverage

---
+++ src/components/Blocks/Hero/View.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import config from '@plone/volto/registry';
+import View from './View';
+
+const observe = jest.fn();
+const unobserve = jest.fn();
+window.IntersectionObserver = jest.fn((callback) => ({
+  observe,
+  unobserve,
+}));
+
+describe('View Component', () => {
+  it('renders view component', () => {
+    config.blocks = {
+      blocksConfig: {
+        hero: {
+          copyrightPrefix: 'Copyright: ',
+        },
+      },
+    };
+    const data = {
+      text: 'Hello, World!',
+      copyright: 'Copyright 2023',
+      copyrightIcon: 'copyright-icon-class',
+      copyrightPosition: 'bottom',
+    };
+    render(<View data={data} />);
+  });
+});
