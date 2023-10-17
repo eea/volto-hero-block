e0bc0c0628c913dea42ce4fb76dd9779b87e6e42
Cretu Mihaela <cretu.mihaelaa11@gmail.com>
2023-10-17 18:25:55 +0300

    fix eslint

--- src/components/Blocks/Hero/View.test.jsx
+++ src/components/Blocks/Hero/View.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import config from '@plone/volto/registry';
 import View from './View';
 
