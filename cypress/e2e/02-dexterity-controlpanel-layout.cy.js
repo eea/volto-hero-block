e4c9e3f1d579826de25fa3ca0b829a1f8a65a744
Crețu Mihaela <68827085+MihaelaCretu11@users.noreply.github.com>
2023-10-18 18:21:08 +0300

    test: add cypress test for hero block and fix error in DX Layout - refs #254894

---
+++ cypress/e2e/02-dexterity-controlpanel-layout.cy.js
@@ -0,0 +1,83 @@
+import { slateLayoutBeforeEach, slateLayoutAfterEach } from '../support/e2e';
+
+describe('ControlPanel: Dexterity Content-Types Layout', () => {
+  beforeEach(slateLayoutBeforeEach);
+  afterEach(slateLayoutAfterEach);
+
+  it('Edit Blocks Layout for Book', () => {
+    cy.visit('/controlpanel/dexterity-types');
+    cy.waitForResourceToLoad('@navigation');
+    cy.waitForResourceToLoad('@breadcrumbs');
+    cy.waitForResourceToLoad('@actions');
+    cy.waitForResourceToLoad('@types');
+
+    cy.get('a[href="/controlpanel/dexterity-types/book"]').should(
+      'have.text',
+      'book',
+    );
+
+    cy.visit('/controlpanel/dexterity-types/book/layout');
+    cy.get('#page-controlpanel-layout').contains(
+      'Can not edit Layout for book',
+    );
+    cy.get('#page-controlpanel-layout button').click();
+
+    // Wait a bit for draftjs to load, without this the title block
+    // custom placeholder is missing and cypress gives a timeout error
+    cy.wait(1000);
+    cy.get('input[id="field-placeholder"]').type('Book title');
+    cy.get('label[for="field-required"]').click();
+    cy.get('label[for="field-fixed"]').click();
+
+    cy.getSlate().click();
+
+    cy.get('.ui.basic.icon.button.block-add-button:visible').click();
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active.common .button.hero')
+      .contains('Hero')
+      .click({ force: true });
+
+    cy.get('#toolbar-save').click();
+
+    cy.visit('/cypress');
+    cy.waitForResourceToLoad('@navigation');
+    cy.waitForResourceToLoad('@breadcrumbs');
+    cy.waitForResourceToLoad('@actions');
+    cy.waitForResourceToLoad('@types');
+
+    cy.get('button[class="add"]').click();
+    cy.get('#toolbar-add-book').click();
+    cy.get('.block.title').contains('Book title');
+    cy.get('.block.hero div[role="presentation"]').click();
+
+    // Add text and button
+    cy.get('.hero-block-text div[role="textbox"]')
+      .click()
+      .type('My hero block');
+    cy.get('label[for="field-fullWidth"]').click();
+    cy.get('label[for="field-fullHeight"]').click();
+    cy.get('label[for="field-quoted"]').click();
+    cy.get('label[for="field-spaced"]').click();
+    cy.get('.field-wrapper-buttonLabel #field-buttonLabel').click().type('Label')
+    cy.get('#field-buttonLabel').click().type('my button');
+    cy.get('.inline.field.field-attached-image .ui.input')
+      .click()
+      .type('https://eea.github.io/volto-eea-design-system/img/eea_icon.png');
+    cy.get(
+      '.inline.field.field-attached-image .ui.buttons .primary.button',
+    ).click();
+
+    // Change book title
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('My First Book');
+    cy.get('.documentFirstHeading').contains('My First Book');
+
+    cy.get('#toolbar-save').click();
+    cy.get('.documentFirstHeading').contains('My First Book');
+    // Check if the button exist
+    cy.get('.hero-block-meta .button').contains('my button');
+    cy.get('.hero-block-image-wrapper');
+    cy.get('.eea.hero-block.spaced.inverted.full-height');
+    cy.get('.hero-block-meta.text-left .button').contains('Label')
+  });
+});
