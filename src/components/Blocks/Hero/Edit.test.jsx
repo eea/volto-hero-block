ad6e98fe433f815a692139d5a06f669c485422ad
dobri1408 <dobriceanionut1408@gmail.com>
2024-03-27 12:16:39 +0200

    fix unit tests

--- src/components/Blocks/Hero/Edit.test.jsx
+++ src/components/Blocks/Hero/Edit.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import Edit from './Edit';
