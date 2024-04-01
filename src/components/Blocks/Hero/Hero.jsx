d560dcb580755767d43d7848f182b420552c1894
dobri1408 <dobriceanionut1408@gmail.com>
2024-04-01 11:59:08 +0300

    fix eslint

--- src/components/Blocks/Hero/Hero.jsx
+++ src/components/Blocks/Hero/Hero.jsx
@@ -38,6 +38,7 @@ function Hero({
   const onScreen = useFirstVisited(bgImgRef);
 
   return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
     <div
       className={cx(
         'eea hero-block',
