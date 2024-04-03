d97f292b189de75085526003179e20dccbf2f979
ana-oprea <anaop3212@gmail.com>
2024-04-03 10:23:27 +0300

    test: add unit tests and delete snapshots

--- src/components/Blocks/Hero/Copyright.test.jsx
+++ src/components/Blocks/Hero/Copyright.test.jsx
@@ -1,35 +1,62 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import Copyright from './Copyright';
+import { render, screen } from '@testing-library/react';
 import { Icon } from 'semantic-ui-react';
 import { Provider } from 'react-intl-redux';
 import configureStore from 'redux-mock-store';
 import config from '@plone/volto/registry';
+import '@testing-library/jest-dom/extend-expect';
+
+import Copyright from './Copyright';
 
 const mockStore = configureStore();
 
-test('renders copyright', () => {
+describe('Copyright', () => {
   const store = mockStore({
     intl: {
       locale: 'en',
       messages: {},
     },
   });
-  const copyrightPrefix =
-    config.blocks.blocksConfig.hero?.copyrightPrefix || '';
+  it('renders copyright with left position', () => {
+    const copyrightPrefix =
+      config.blocks.blocksConfig.hero?.copyrightPrefix || '';
 
-  const component = renderer.create(
-    <Provider store={store}>
-      <Copyright copyrightPosition={'left'}>
-        <Copyright.Prefix>{copyrightPrefix}</Copyright.Prefix>
-        <Copyright.Icon>
-          <Icon className={'copyright outline'} />
-        </Copyright.Icon>
-        <Copyright.Text>Copyright Text Test</Copyright.Text>
-      </Copyright>
-    </Provider>,
-  );
+    const { container } = render(
+      <Provider store={store}>
+        <Copyright copyrightPosition={'left'}>
+          <Copyright.Prefix>{copyrightPrefix}</Copyright.Prefix>
+          <Copyright.Icon>
+            <Icon className={'copyright outline'} />
+          </Copyright.Icon>
+          <Copyright.Text>Copyright Text Test</Copyright.Text>
+        </Copyright>
+      </Provider>,
+    );
 
-  const json = component.toJSON();
-  expect(json).toMatchSnapshot();
+    expect(screen.getByText('Copyright Text Test')).toBeInTheDocument();
+    expect(container.querySelector('i.copyright.outline')).toBeInTheDocument();
+    expect(
+      container.querySelector('div.eea.copyright.left'),
+    ).toBeInTheDocument();
+  });
+
+  it('renders copyright without prefix', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Copyright copyrightPosition={'right'}>
+          <Copyright.Icon>
+            <Icon className={'copyright outline'} />
+          </Copyright.Icon>
+          <Copyright.Text>Copyright Text Test</Copyright.Text>
+        </Copyright>
+      </Provider>,
+    );
+
+    expect(screen.queryByText('Prefix')).not.toBeInTheDocument();
+    expect(screen.getByText('Copyright Text Test')).toBeInTheDocument();
+    expect(container.querySelector('i.copyright.outline')).toBeInTheDocument();
+    expect(
+      container.querySelector('div.eea.copyright.right'),
+    ).toBeInTheDocument();
+  });
 });
