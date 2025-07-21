cb046f9327ec69c2d6ea64c53a2b256f95a6a0ac
David Ichim <ichim.david@gmail.com>
2025-07-21 13:24:04 +0300

    removed useEffect fix as it broke tests by no longer showing the hero sidebar options

--- src/components/Blocks/Hero/Edit.jsx
+++ src/components/Blocks/Hero/Edit.jsx
@@ -63,39 +63,35 @@ export default function Edit(props) {
   const blockState = {};
   const data_blocks = data?.data?.blocks;
 
-  // Move the data migration logic to useEffect to avoid setState during render
-  // this happens when the component is first rendered such as on block creation
-  React.useEffect(() => {
-    if (data?.text || isEmpty(data_blocks)) {
-      let dataWithoutText = { ...data };
-      if (dataWithoutText) delete dataWithoutText.text;
+  if (data?.text || isEmpty(data_blocks)) {
+    let dataWithoutText = { ...data };
+    if (dataWithoutText) delete dataWithoutText.text;
 
-      onChangeBlock(block, {
-        ...dataWithoutText,
-        data: data?.text
-          ? {
-              blocks: {
-                [id]: {
-                  '@type': 'slate',
-                  value: data.text,
-                  plaintext: data.text?.[0].children?.[0].text,
-                },
+    onChangeBlock(block, {
+      ...dataWithoutText,
+      data: data?.text
+        ? {
+            blocks: {
+              [id]: {
+                '@type': 'slate',
+                value: data.text,
+                plaintext: data.text?.[0].children?.[0].text,
               },
-              blocks_layout: { items: [id] },
-            }
-          : {
-              blocks: {
-                [id]: {
-                  '@type': 'slate',
-                  value: [{ type: 'h2', children: [{ text: '' }] }],
-                  plaintext: '',
-                },
-              },
-              blocks_layout: { items: [id] },
             },
-      });
-    }
-  }, [data?.text, data_blocks, id, onChangeBlock, block, data]);
+            blocks_layout: { items: [id] },
+          }
+        : {
+            blocks: {
+              [id]: {
+                '@type': 'slate',
+                value: [{ type: 'h2', children: [{ text: '' }] }],
+                plaintext: '',
+              },
+            },
+            blocks_layout: { items: [id] },
+          },
+    });
+  }
 
   return (
     <>
